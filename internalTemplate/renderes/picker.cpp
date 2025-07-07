a42e5c86f3c41db2558f3afff226e9c41b4b4651
moritz <hanshret@proton.me>
2025-07-07 15:02:08 +0200

    wrapper functions for draw calls, so its just one func for every renderer

--- internalTemplate/renderes/picker.cpp
+++ internalTemplate/renderes/picker.cpp
@@ -44,8 +44,148 @@ bool CreateRenderer(RendererOptions renderer)
     }
 }
 
-
-void Render(const std::function<void()>& fn)
+void DrawLine(float x1, float y1, float x2, float y2, float lineWidth, const unsigned char color[3])
 {
-    RenderQueue::Submit(fn);
+    switch (Renderer)
+    {
+    case RendererOptions::NONE:
+        return;
+
+    case RendererOptions::DX9:
+        return;
+
+    case RendererOptions::DX10:
+        return;
+
+    case RendererOptions::DX11:
+        return;
+
+    case RendererOptions::DX12:
+        return;
+
+    case RendererOptions::OPENGL:
+        return RenderQueue::Submit([x1, y1, x2, y2, lineWidth, color] {GL::DrawLine(x1, y1, x2, y2, lineWidth, color); });
+
+    case RendererOptions::VULKAN:
+        return;
+    }
+}
+
+void DrawFilledRect(float x, float y, float width, float height, const unsigned char color[3])
+{
+    switch (Renderer)
+    {
+    case RendererOptions::NONE:
+        return;
+
+    case RendererOptions::DX9:
+        return;
+
+    case RendererOptions::DX10:
+        return;
+
+    case RendererOptions::DX11:
+        return;
+
+    case RendererOptions::DX12:
+        return;
+
+    case RendererOptions::OPENGL:
+        return RenderQueue::Submit([x, y, width, height, color] {GL::DrawFilledRect(x, y, width, height, color); });
+
+    case RendererOptions::VULKAN:
+        return;
+    }
+}
+
+void DrawOutline(float x, float y, float width, float height, float lineWidth, const unsigned char color[3])
+{
+    switch (Renderer)
+    {
+    case RendererOptions::NONE:
+        return;
+
+    case RendererOptions::DX9:
+        return;
+
+    case RendererOptions::DX10:
+        return;
+
+    case RendererOptions::DX11:
+        return;
+
+    case RendererOptions::DX12:
+        return;
+
+    case RendererOptions::OPENGL:
+        return RenderQueue::Submit([x, y, width, height, lineWidth, color] {GL::DrawOutline(x, y, width, height, lineWidth, color); });
+
+    case RendererOptions::VULKAN:
+        return;
+    }
+}
+
+void DrawCircle(float cx, float cy, float r, int num_segments, GLfloat lineWidth, const unsigned char color[3])
+{
+    switch (Renderer)
+    {
+    case RendererOptions::NONE:
+        return;
+
+    case RendererOptions::DX9:
+        return;
+
+    case RendererOptions::DX10:
+        return;
+
+    case RendererOptions::DX11:
+        return;
+
+    case RendererOptions::DX12:
+        return;
+
+    case RendererOptions::OPENGL:
+        return RenderQueue::Submit([cx, cy, r, num_segments, lineWidth, color] {GL::DrawCircle(cx, cy, r, num_segments, lineWidth, color); });
+
+    case RendererOptions::VULKAN:
+        return;
+    }
+}
+
+void Print(float x, float y, const unsigned char color[3], const char* format, ...)
+{
+    char Buffer[100];
+
+    va_list args;
+    va_start(args, format);
+    vsnprintf(Buffer, 100, format, args);
+    va_end(args);
+
+    switch (Renderer)
+    {
+    case RendererOptions::NONE:
+        return;
+
+    case RendererOptions::DX9:
+        return;
+
+    case RendererOptions::DX10:
+        return;
+
+    case RendererOptions::DX11:
+        return;
+
+    case RendererOptions::DX12:
+        return;
+
+    case RendererOptions::OPENGL:
+    {
+        return RenderQueue::Submit([=]() {
+            GL::Print(x, y, color, format, Buffer);
+            });
+    }
+
+    case RendererOptions::VULKAN:
+        return;
+    }
 }
--- internalTemplate/renderes/picker.h
+++ internalTemplate/renderes/picker.h
@@ -2,6 +2,9 @@
 #include <mutex>
 #include <vector>
 #include <functional>
+#include <cstdarg>
+#include <cstdio>
+#include <string>
 #include "d3d9/crd3d9.h"
 #include "opengl/glhook.h"
 #include "RendererOptions.h"
@@ -17,4 +20,8 @@ namespace RenderQueue
 
 bool CreateRenderer(RendererOptions renderer);
 
-void Render(const std::function<void()>& fn);
+void DrawLine(float x1, float y1, float x2, float y2, float lineWidth, const unsigned char color[3]);
+void DrawFilledRect(float x, float y, float width, float height, const unsigned char color[3]);
+void DrawOutline(float x, float y, float width, float height, float lineWidth, const unsigned char color[3]);
+void DrawCircle(float cx, float cy, float r, int num_segments, GLfloat lineWidth, const unsigned char color[3]);
+void Print(float x, float y, const unsigned char color[3], const char* format, ...);
